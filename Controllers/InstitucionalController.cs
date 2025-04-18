8faae8b6bb331553a491737617cb87449fe91d75
meCapel <meccapelani@gmail.com>
2025-04-18 19:08:11 -0300

    setup inicial, view e controller home

---
+++ Controllers/InstitucionalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projeto_integrador.Controllers
+{
+    public class InstitucionalController : Controller
+    {
+        public IActionResult Suporte()
+        {
+            return View();
+        }
+        public IActionResult Sobre_nos()
+        {
+            return View();
+        }
+    }
+}
